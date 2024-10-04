0b3edcbb17a0cc097900c4d03a742637cf8ca60a
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-10-04 18:36:29 +0200

    commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.support_01_preguntados import *
+
+def main():
+    object = Mascota("rojo","gato",3,4,"pekines")
+    print(object.color)
+
+if __name__ == "__main__":
+   main()
+
+# funcion mostrar menu
+
+# funcion main
+#while true
--- src/main.py
+++
@@ -1,5 +0,0 @@
-from funciones import *
-
-
-object = Mascota("rojo","gato",3,4,"pekines")
-print(object.color)
