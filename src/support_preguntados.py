b6f5278e0bb9eb8281685d13dc3f570400d4cb17
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-10-05 20:05:29 +0200

    excepciones

--- src/support_preguntados.py
+++ src/support_preguntados.py
@@ -9,42 +9,54 @@ class Preguntados_Support:
     num_para_victorias = 10
 
     def read_data_file(self):
-
-        with open('./src/datos.txt', 'r',-1,"utf-8") as archivo:
-            contenido = archivo.read()
-            return ast.literal_eval(contenido)
+        try:
+            with open('./src/datos.txt', 'r',-1,"utf-8") as archivo:
+                contenido = archivo.read()
+                return ast.literal_eval(contenido)
+        except:
+            print("Error al abrir el fichero con los datos de las preguntas")
     
     def todas_las_preguntas(self, input_data, categorias):
+        try:
+            preguntas = []
 
-        preguntas = []
+            for categoria in categorias:
+                if (input_data.get(categoria)) != None:
+                    for pregunta in input_data.get(categoria):
+                        preguntas.append(pregunta)
 
-        for categoria in categorias:
-            if (input_data.get(categoria)) != None:
-                for pregunta in input_data.get(categoria):
-                    preguntas.append(pregunta)
-
-        return preguntas
+            return preguntas
+        except:
+            print("Error al extraer todas las preguntas")
     
     def list_random_preguntas(self, list_preguntas,num_random):
-        return random.choices(list_preguntas,k=num_random)
+        try:
+            return random.choices(list_preguntas,k=num_random)
+        except:
+            print("Error al sacar la lista random de preguntas")
     
     def extrae_respuestas_pregunta(self, pregunta):
+        try:
+            result = []
+            for clave, valor in pregunta.items():
+                if(isinstance(valor, dict)):
+                    for clave_sub, valor_sub in valor.items():
+                        result.append((clave_sub,valor_sub))
 
-        result = []
-        for clave, valor in pregunta.items():
-            if(isinstance(valor, dict)):
-                for clave_sub, valor_sub in valor.items():
-                    result.append((clave_sub,valor_sub))
-
-        return result
+            return result
+        except:
+            print("Error al extraer las respuestas de las pregunta")
     
     def input_data_valida(self):
-        dato = input()
-        dato = dato.lower()
-        if dato == 'a' or dato == 'b' or dato == 'c' or  dato == 'd':
-            return dato
-        else:
-            return 'z'
+        try:
+            dato = input()
+            dato = dato.lower()
+            if dato == 'a' or dato == 'b' or dato == 'c' or  dato == 'd':
+                return dato
+            else:
+                return 'z'
+        except:
+            print("Error al capturar y validar la respuesta de la pregunta")
         
 
     def mostrar_menu(self):
